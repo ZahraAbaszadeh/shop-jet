76705a2df43796a56b50ff78b910f7fc2ea6c26d
ZahraAbaszadeh <zahraabaszade96@gmail.com>
2023-04-23 22:40:38 +0430

    add navbar

--- src/components/CartButtons.js
+++ src/components/CartButtons.js
@@ -1,14 +1,27 @@
-import React from 'react'
-import { FaShoppingCart, FaUserMinus, FaUserPlus } from 'react-icons/fa'
-import { Link } from 'react-router-dom'
-import styled from 'styled-components'
-import { useProductsContext } from '../context/products_context'
-import { useCartContext } from '../context/cart_context'
-import { useUserContext } from '../context/user_context'
+import React from "react";
+import { FaShoppingCart, FaUserMinus, FaUserPlus } from "react-icons/fa";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+import { useProductsContext } from "../context/products_context";
+import { useCartContext } from "../context/cart_context";
+import { useUserContext } from "../context/user_context";
 
 const CartButtons = () => {
-  return <h4>cart buttons </h4>
-}
+  return (
+    <Wrapper className="cart-btn-wrapper">
+      <Link to="/cart" className="cart-btn">
+        Cart
+        <span className="cart-container">
+          <FaShoppingCart />
+          <span className="cart-value">15</span>
+        </span>
+      </Link>
+      <button type="button" className="auth-btn">
+        Login <FaUserPlus/>
+      </button>
+    </Wrapper>
+  );
+};
 
 const Wrapper = styled.div`
   display: grid;
@@ -17,10 +30,9 @@ const Wrapper = styled.div`
   width: 225px;
 
   .cart-btn {
-    color: var(--clr-grey-1);
-    font-size: 1.5rem;
-    letter-spacing: var(--spacing);
-    color: var(--clr-grey-1);
+    font-size: 1.2rem;
+    font-weight: var(--font-weight-bold);
+    color: var(--clr-grey-3);
     display: flex;
 
     align-items: center;
@@ -30,22 +42,22 @@ const Wrapper = styled.div`
     align-items: center;
     position: relative;
     svg {
-      height: 1.6rem;
+      height: 1.5rem;
       margin-left: 5px;
     }
   }
   .cart-value {
     position: absolute;
-    top: -10px;
-    right: -16px;
-    background: var(--clr-primary-5);
-    width: 16px;
-    height: 16px;
+    top: -15px;
+    right: -19px;
+    background: var(--clr-green-dark);
+    width: 14px;
+    height: 14px;
     display: flex;
     align-items: center;
     justify-content: center;
     border-radius: 50%;
-    font-size: 0.75rem;
+    font-size: 0.8rem;
     color: var(--clr-white);
     padding: 12px;
   }
@@ -54,13 +66,13 @@ const Wrapper = styled.div`
     align-items: center;
     background: transparent;
     border-color: transparent;
-    font-size: 1.5rem;
+    font-size: 1.2rem;
     cursor: pointer;
-    color: var(--clr-grey-1);
-    letter-spacing: var(--spacing);
+    color: var(--clr-grey-3);
+    font-weight: var(--font-weight-bold);
     svg {
       margin-left: 5px;
     }
   }
-`
-export default CartButtons
+`;
+export default CartButtons;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,20 +1,37 @@
-import React from 'react'
-import styled from 'styled-components'
-import logo from '../assets/logo.svg'
-import { FaBars } from 'react-icons/fa'
-import { Link } from 'react-router-dom'
-import { links } from '../utils/constants'
-import CartButtons from './CartButtons'
-import { useProductsContext } from '../context/products_context'
-import { useUserContext } from '../context/user_context'
+import React from "react";
+import styled from "styled-components";
+import logo from "../assets/shop-jet-logo.png";
+import { FaBars } from "react-icons/fa";
+import { Link } from "react-router-dom";
+import { links } from "../utils/constants";
+import CartButtons from "./CartButtons";
+import { useProductsContext } from "../context/products_context";
+import { useUserContext } from "../context/user_context";
 
 const Nav = () => {
   return (
     <NavContainer>
-      <div className="nav-center"></div>
+      <div className="nav-center">
+        <div className="nav-header">
+          <Link to="/">
+            <img src={logo} alt="LOGO" />
+          </Link>
+          <button type="button" className="nav-toggle">
+            <FaBars />
+          </button>
+        </div>
+        <ul className="nav-links">
+          {links.map((link) => (
+            <li key={link.id}>
+              <Link to={link.url}>{link.text}</Link>
+            </li>
+          ))}
+        </ul>
+        <CartButtons/>
+      </div>
     </NavContainer>
   );
-}
+};
 
 const NavContainer = styled.nav`
   height: 5rem;
@@ -32,14 +49,14 @@ const NavContainer = styled.nav`
     align-items: center;
     justify-content: space-between;
     img {
-      width: 175px;
+      width: 140px;
       margin-left: -15px;
     }
   }
   .nav-toggle {
     background: transparent;
     border: transparent;
-    color: var(--clr-primary-5);
+    color: var(--clr-grey-4);
     cursor: pointer;
     svg {
       font-size: 2rem;
@@ -68,12 +85,12 @@ const NavContainer = styled.nav`
       }
       a {
         color: var(--clr-grey-3);
-        font-size: 1rem;
+        font-size: 1.1rem;
+        font-weight: var(--font-weight-bold);
         text-transform: capitalize;
-        letter-spacing: var(--spacing);
-        padding: 0.5rem;
+        padding: 0.4rem;
         &:hover {
-          border-bottom: 2px solid var(--clr-primary-7);
+          border-bottom: 2px solid var(--clr-grey-4);
         }
       }
     }
@@ -81,6 +98,6 @@ const NavContainer = styled.nav`
       display: grid;
     }
   }
-`
+`;
 
-export default Nav
+export default Nav;
